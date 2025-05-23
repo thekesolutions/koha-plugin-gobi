ebd3ccdbbb9977a9cc20a60e2513081dd2ae04fb
Tomas Cohen Arazi <tomascohen@theke.io>
2025-05-23 10:55:03 -0300

    3.1.1

--- Koha/Plugin/Com/Theke/GOBI.pm
+++ Koha/Plugin/Com/Theke/GOBI.pm
@@ -52,7 +52,7 @@ use Try::Tiny;
 use MARC::Record;
 
 ## Here we set our plugin version
-our $VERSION = "3.1.0";
+our $VERSION = "3.1.1";
 
 our $metadata = {
     name            => 'GOBI integration',
